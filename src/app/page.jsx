20c61121c2a3b551cc4c5f061e60e1c55295291e
Josue Benavides <jdbs9514@gmail.com>
2024-02-21 00:31:41 -0500

    added the home title

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col bg-zinc-800 h-screen">
+      <h1 className="head_text text-center">
+        Discover & share
+        <br className="max-md:hidden" />
+        <span className="blue_gradient text-center">
+          crazy theories about your favorite animes
+        </span>
+      </h1>
+      <p className="desc text-center">
+        opiniON is a platform where you can share your theories about your favorite animes and discover what other people think about them.
+      </p>
+    </section>
+  )
+}
+
+export default Home
